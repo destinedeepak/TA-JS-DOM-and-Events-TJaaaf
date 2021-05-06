8888a48e4826411b5fcec70121c4b108547dca41
Deepak Singh <destinedks@gmail.com>
2021-05-06 22:22:16 +0530

    add overlay_modal

--- block-BJaaer/code/script.js
+++ block-BJaaer/code/script.js
@@ -1,4 +1,5 @@
 let form = document.querySelector('form');
+let display = document.querySelector('.display');
 
 
 let  userInfo = {}
@@ -15,15 +16,20 @@ function handler(event){
     userInfo.terms = form.elements.terms.checked;
     console.log(userInfo)
 
-    document.querySelector('h1').innerText += userInfo.name;
-    document.querySelector('.email').innerText += userInfo.email;
-    document.querySelector('.interest').innerText += userInfo.movie;
-    document.querySelector('.color').innerText += userInfo.color;
-    document.querySelector('.rating').innerText += userInfo.rateOfMovie;
-    document.querySelector('.genre').innerText += userInfo.bookGenre;
+    document.querySelector('h1').innerText = `Hello ${userInfo.name}`;
+    document.querySelector('.email').innerText = `Email: ${userInfo.email}`;
+    document.querySelector('.interest').innerText = `Your love: ${userInfo.movie}`;
+    document.querySelector('.color').innerText = `Color: ${userInfo.color}`;
+    document.querySelector('.rating').innerText = `Rating: ${userInfo.rateOfMovie}`;
+    document.querySelector('.genre').innerText = `Book Genre: ${userInfo.bookGenre}`;
     document.querySelector('.term').innerText = 
-    `You are ${userInfo.terms? 'agreed' : 'not agreed to Terms and Conditions'}` ;
+    `You are ${userInfo.terms ? 'agreed' : 'not agreed'} to Terms and Conditions` ;
+
+    display.classList.add('open')
 }
+
+display.addEventListener('click',function(){display.classList.remove('open')})
+
 
 
 form.addEventListener('submit',handler);
